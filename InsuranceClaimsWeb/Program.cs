96bcabc5a051fe83e64407ff902e3ef9dd416caf
Raul Bastian <raulbastian@outlook.com>
2022-10-23 18:32:27 +0200

    Obtaining values from an appsettings key: - From appsettings.json - Appsettings.development.json - Environment variable

--- InsuranceClaimsWeb/Program.cs
+++ InsuranceClaimsWeb/Program.cs
@@ -3,6 +3,10 @@ using System.Diagnostics;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => $"Hello World! from {Process.GetCurrentProcess().ProcessName}");
+app.MapGet("/", (IConfiguration configuration) =>
+{
+   var myValue = configuration.GetValue<string>("MyKey");
+   return  $"Hello World! from {Process.GetCurrentProcess().ProcessName} and key MyKey={myValue} from app settings";
+});
 
 app.Run();
